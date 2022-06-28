1cdd14e9f889df2ee2fa72e2d8e43a20c1d27cc6
Danil <mydogfromhell666@gmail.com>
2022-06-28 13:08:46 +0300

    add response code 500, user already register

--- script/app.js
+++ script/app.js
@@ -4,7 +4,7 @@ const username = document.getElementById('username')
 const password = document.getElementById('pass')
 const email = document.getElementById('email')
 
-sub.addEventListener('click', async function(){
+sub.addEventListener('click', async function () {
     const data = {
         fname: fname.value,
         username: username.value,
@@ -13,9 +13,21 @@ sub.addEventListener('click', async function(){
     }
     const res = fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
-        headers:{
-            'Access-Control-Allow-Origin': '*'
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+
         },
         body: JSON.stringify(data)
     })
+        .then((res) => {
+            if (res.status === 500) {
+                const regAlert = document.querySelector('.reg__alert')
+                regAlert.innerHTML = ""
+                regAlert.insertAdjacentHTML('beforeend', '<p style="text-align:center; color: #a82a38;font-size: 16px; margin-top: 20px;">Пользователь уже существует!</p>')
+
+            }
+        })
+
+
 })
